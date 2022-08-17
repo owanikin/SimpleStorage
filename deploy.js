6089f933b856541512616d5c634fb326cad189a4
Owanikin <oderindeife@gmail.com>
2022-08-17 12:22:23 +0100

    encrption

--- deploy.js
+++ deploy.js
@@ -6,10 +6,9 @@ async function main() {
      
     // http://127.0.0.1:7545
     const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
-    const wallet = new ethers.Wallet(
-        process.env.PRIVATE_KEY,
-        provider
-    );
+    const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
+    let wallet = new ethers.Wallet.fromEncryptedJsonSync(encryptedJson, process.env.PRIVATE_KEY_PASSWORD);
+    wallet = await wallet.connect(provider)
     const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf-8");
     const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf-8");
      const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
